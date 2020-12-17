20bb88af77194446285cadf889c0be946b7b3fea
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-12-17 14:50:52 +0100

    get api data

--- 2-redux-search/src/components/SearchBar.js
+++ 2-redux-search/src/components/SearchBar.js
@@ -6,17 +6,23 @@ import {
     FormControl
 } from 'react-bootstrap'
 
+import {getData} from '../services'
 
 class SearchBar extends Component {
     state = {
         searchWord: ''
+    }
+    searchBtnClick = () => {
+        getData(this.state.searchWord).then(data => {
+            console.log(data.hits);
+        })
     }
     render() {
         return (
             <div className="row mt-3">
             <InputGroup className="mb-3">
                 <InputGroup.Prepend>
-                    <Button variant="outline-secondary">Search</Button>
+                    <Button onClick={this.searchBtnClick} variant="outline-secondary">Search</Button>
                 </InputGroup.Prepend>
                 <FormControl 
                 value={this.state.searchWord} 
