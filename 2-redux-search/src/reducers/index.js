ef49d3adce73b32b0861b4c918de5bdad9e021ed
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-12-17 15:41:10 +0100

    create reducers file

---
+++ 2-redux-search/src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux'
+
+const resultReducer = (results = [], action) => {
+
+
+
+    return results
+}
+
+export default combineReducers({
+    results: resultReducer
+})
